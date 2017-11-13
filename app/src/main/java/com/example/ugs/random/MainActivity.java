fc0de76d04b07636fed7062226f151fd98045768
iceval <valera.2001@mail.ru>
2017-11-13 19:38:52 +0300

    plus and minus

--- app/src/main/java/com/example/ugs/random/MainActivity.java
+++ app/src/main/java/com/example/ugs/random/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.ugs.random;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void plus(View v){
+        EditText num1 =
+                (EditText) this.findViewById(R.id.num1);
+        String num1text = num1.getText().toString();
+        EditText num2 =
+                (EditText) this.findViewById(R.id.num2);
+        String num2text = num2.getText().toString();
+        TextView out =
+                (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text) + Double.parseDouble(num2text)));
+    }
+
+    public void minus(View view) {
+
     }
 }
