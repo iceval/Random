fd373cca2125fe8014b085826843bcd8f6d2a21b
iceval <valera.2001@mail.ru>
2017-11-17 20:04:38 +0300

    main5

--- app/src/main/java/com/example/ugs/random/MainActivity.java
+++ app/src/main/java/com/example/ugs/random/MainActivity.java
@@ -26,6 +26,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void minus(View view) {
-
+        EditText num1 =
+                (EditText) this.findViewById(R.id.num1);
+        String num1text = num1.getText().toString();
+        EditText num2 =
+                (EditText) this.findViewById(R.id.num2);
+        String num2text = num2.getText().toString();
+        TextView out =
+                (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text) - Double.parseDouble(num2text)));
+    }
+    public void multiply(View v){
+        EditText num1 =
+                (EditText) this.findViewById(R.id.num1);
+        String num1text = num1.getText().toString();
+        EditText num2 =
+                (EditText) this.findViewById(R.id.num2);
+        String num2text = num2.getText().toString();
+        TextView out =
+                (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text) * Double.parseDouble(num2text)));
+    }
+    public void divide(View v){
+        EditText num1 =
+                (EditText) this.findViewById(R.id.num1);
+        String num1text = num1.getText().toString();
+        EditText num2 =
+                (EditText) this.findViewById(R.id.num2);
+        String num2text = num2.getText().toString();
+        TextView out =
+                (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text) / Double.parseDouble(num2text)));
     }
 }
